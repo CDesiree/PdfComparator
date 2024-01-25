22d14f20711fb4a9c611d843843afc5e7ccd91c8
CDesiree <desireecastrocpe@gmail.com>
2024-01-25 23:14:35 +0800

    [feat] change parameter from string to multipart file

--- src/main/java/com/dpmc/pdfcompare/pdfcompare/PdfController.java
+++ src/main/java/com/dpmc/pdfcompare/pdfcompare/PdfController.java
@@ -2,11 +2,13 @@ package com.dpmc.pdfcompare.pdfcompare;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("api/pdf")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PdfController {
     private final PdfService pdfService;
 
@@ -16,9 +18,13 @@ public class PdfController {
     }
 
     @PostMapping("/compare")
-    public String comparePdf(@RequestBody PdfRequest pdfRequest) throws IOException {
-        return pdfService.comparePdfs(pdfRequest);
-    }
+    public String comparePdf(
+            @RequestParam("file1") MultipartFile file1,
+            @RequestParam("file2") MultipartFile file2
+            ) throws IOException {
+                String resultFile = pdfService.comparePdfs(file1, file2);
+                return resultFile;
+            }
     @GetMapping("/isAlive")
     public String isAlive() {
         return "api is alive~";
