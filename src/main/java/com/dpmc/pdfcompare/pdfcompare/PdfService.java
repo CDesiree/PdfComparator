6970f507598dd6e339b19740c9787c6b0d38ca70
CDesiree <desireecastrocpe@gmail.com>
2024-01-25 23:15:39 +0800

    [feat] change service method to process multipart file

--- src/main/java/com/dpmc/pdfcompare/pdfcompare/PdfService.java
+++ src/main/java/com/dpmc/pdfcompare/pdfcompare/PdfService.java
@@ -4,22 +4,51 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 import de.redsix.pdfcompare.PdfComparator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 @Service
 public class PdfService {
 
-    public String comparePdfs(PdfRequest pdfRequest) throws IOException {
-        String path1 = pdfRequest.getPath1();
-        String path2 = pdfRequest.getPath2();
+    public String comparePdfs(
+            MultipartFile file1,
+            MultipartFile file2
+    ) throws IOException {
+        String path1 = saveFile(file1);
+        String path2 = saveFile(file2);
+
         String resultFile = "D:\\pdfcompareapp\\files\\Compared";
 
-        PdfComparator pdfComparator = new PdfComparator(path1, path2);
-        boolean isEquals = pdfComparator.compare().writeTo(resultFile);
+        try {
+            PdfComparator pdfComparator = new PdfComparator(path1, path2);
+            boolean isEquals = pdfComparator.compare().writeTo(resultFile);
 
-        System.out.println("Is Equal: " + isEquals);
-        System.out.println("Process complete");
+            System.out.println("Is equal: " + isEquals);
+            System.out.println("Process complete");
 
-        return resultFile;
+            return resultFile;
+        } finally {
+            deleteFile(path1);
+            deleteFile(path2);
+        }
+    }
+
+    private String saveFile(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        String filePath = "D:\\pdfcompareapp\\files\\" + fileName;
+
+        Files.copy(file.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+
+        return filePath;
+    }
+
+    private void deleteFile(String filePath) throws IOException {
+        // Delete the file at the specified path
+        Files.deleteIfExists(Paths.get(filePath));
     }
 }
