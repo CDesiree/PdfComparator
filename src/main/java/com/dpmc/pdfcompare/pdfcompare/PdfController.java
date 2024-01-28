5d76e347463f52d761c4aa001c927a3321d48ea2
CDesiree <desireecastrocpe@gmail.com>
2024-01-28 21:04:09 +0800

    [feat] add api to download file

--- src/main/java/com/dpmc/pdfcompare/pdfcompare/PdfController.java
+++ src/main/java/com/dpmc/pdfcompare/pdfcompare/PdfController.java
@@ -1,16 +1,24 @@
 package com.dpmc.pdfcompare.pdfcompare;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("api/pdf")
 @CrossOrigin(origins = "http://localhost:3000")
 public class PdfController {
     private final PdfService pdfService;
+    public String RESULT_FILE_PATH = "D:\\pdfcompareapp\\files\\Compared.pdf";
 
     @Autowired
     public PdfController(PdfService pdfService) {
@@ -25,9 +33,26 @@ public class PdfController {
                 String resultFile = pdfService.comparePdfs(file1, file2);
                 return resultFile;
             }
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> downloadPdf() throws IOException {
+        String resultFilePath = RESULT_FILE_PATH;
+        File file = new File(resultFilePath);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", file.getName());
+
+        Path path = Paths.get(file.getPath());
+        byte[] fileContent = Files.readAllBytes(path);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(fileContent);
+    }
     @GetMapping("/isAlive")
     public String isAlive() {
         return "api is alive~";
     }
 
 }
+
